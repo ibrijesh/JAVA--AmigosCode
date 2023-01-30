0bb3dac5187aa5c5e36631e1deb153327ef9c221
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-01-30 12:44:19 +0530

    Static v/s Non-static Data Member Access Inside Same Class , In Different Class and In Different Package.

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,7 +1,32 @@
 package com.amigoscode;
 
 public class Main {
+
+    public static String foo = "foo-bar";   // static data member, its part of class no Instance, therefore we can access it without  creating Object.
+
+    public String fuu = "fuu-bar";    // non-static data member, its part of class Instance, therefore we have to   create Object to access it.
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // for foo (static data member), we do not need to create object.
+        System.out.println(
+                Main.foo
+        );
+
+                   // OR  //
+
+        System.out.println(
+                foo
+        );
+
+
+        // for fuu (non-static data member). we have to create object of class
+        Main main = new Main();
+
+        System.out.println(
+                main.fuu
+        );
+
     }
+
 }
---
+++ src/com/amigoscode/foo/Apple.java
@@ -0,0 +1,20 @@
+package com.amigoscode.foo;
+
+import com.amigoscode.Main;
+
+public class Apple {
+    public static void main(String[] args) {
+
+        // for foo (static data member), we do not need to create object.
+        System.out.println(
+                Main.foo
+        );
+
+        // for fuu (non-static data member). we have to create object of class
+        Main main = new Main();
+
+        System.out.println(
+                main.fuu
+        );
+    }
+}
