e704bc03f042da70122fd9141b80406718a2d9bd
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-01-31 14:04:24 +0530

    Final keyword
    Its like const in c++ but it comes with limited usage compared to c++.

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,27 +1,29 @@
 package com.amigoscode;
 
-import java.util.Arrays;
+import java.awt.*;
 
 public class Main {
 
-    // static var foo="bar";   var not allowed
+    final static String foo = "bar";
 
     public static void main(String[] args) {
-        // Type Inference with var keyword , it works with local variable only //
+        // final keyword , its like const in c++ but it with limited usage compared to c++
 
-        var name = "Brijesh";
-        var names = new String[]{"CR7", "CJ7", "Ram", "Bheem", "Raju"};
-        var age=22;
-        var balance =1_000_000.33;
+        // foo = "Hello"; not allowed
 
+        final var name = "Brijesh";
+        //  name="Rahul" not allowed
 
+        final Point point = new Point(2, 4);
+        //point = new Point(4, 5) not allowed because class object is declared as final.
+        point.x = 4;
+        point.y = 6;
+        // allowed because variable are declared final in class Point
 
+        System.out.println(foo);
         System.out.println(name);
-        System.out.println(
-                Arrays.toString(names)
-        );
-        System.out.println(age);
-        System.out.println(balance);
+        System.out.println(point);
+
 
     }
 
