d493ab0455e0b80b1e20931501e8d88757936858
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-02 18:35:38 +0530

    Strings - String Literal vs String Object.
    String Literal - stored in String Pool (specially created by JVM) in Heap.
    
    String Object - stored in normal Heap.

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -4,23 +4,31 @@ package com.amigoscode;
 public class Main {
 
     public static void main(String[] args) {
-       // Strings //
+        // Strings //
 
-        /* These are String Literal , they are referenced in String Pool */
+        /* These are String Literal , they are referenced in String Pool (specially created by JVM) in Heap */
 
-        String name1="Jamila";
-        String name2="Jamila";
+        String name1 = "Jamila";
+        String name2 = "Jamila";
 
         // both are pointing(referencing) to same memory location in String Pool.  (see photos)
-        System.out.println(name1.hashCode());
-        System.out.println(name2.hashCode());
+        if (name1 == name2)                   // reference to same memory location.
+            System.out.println("Same");
+        else
+            System.out.println("Different");
 
 
-        name2="Alex";
 
-        // now both are pointing(referencing) to Different memory location in String Pool. (see photos)
-        System.out.println(name1.hashCode());
-        System.out.println(name2.hashCode());
+        /* These are String  Object , they reference in normal Heap. */
+
+        String name3 = new String("Brijesh");
+        String name4 = new String("Brijesh");
+
+        // now both are pointing(referencing) to Different memory location in normal  Heap. (see photos)
+        if (name3 == name4)                   // reference to different memory location.
+            System.out.println("Same");
+        else
+            System.out.println("Different");
 
     }
 
