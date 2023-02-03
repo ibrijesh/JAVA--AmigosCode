1f843dc009d7c4e90e8c0ce6f4acf7bae11e820c
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-03 14:20:06 +0530

    Dates - Working with LocalDateTime Class

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,33 +1,22 @@
 package com.amigoscode;
 
+import java.time.LocalDateTime;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Wrapper Classes //
-          /*
-            Primitive Type	  Wrapper class
-                boolean	        Boolean
-                char	        Character
-                byte	        Byte
-                short	        Short
-                int	            Integer
-                long	        Long
-                float	        Float
-                double	        Double
-           */
+        // Working with Dates
 
-        Integer age = 33;
-        Double price = 206.77;
-        var ageStr = "55";
-        int a = Integer.parseInt(ageStr);
+        LocalDateTime now = LocalDateTime.now();
 
-        System.out.println(a);
-        System.out.println(Integer.min(-7,8));
-
-    }
-
-    public static void change(Integer age){
+        System.out.println(now);
+        System.out.println(now.getDayOfMonth());
+        System.out.println(now.getDayOfWeek());
+        System.out.println(now.getDayOfYear());
+        System.out.println(now.getHour());
+        System.out.println(now.getMinute());
+        System.out.println(now.getSecond());
+        System.out.println(now.minusDays(5));
 
     }
 
