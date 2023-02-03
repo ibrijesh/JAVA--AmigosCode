29394b1bf3a2f13974916c33478cba83a91733e0
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-03 15:19:48 +0530

    Dates - Working with ZoneId

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,44 +1,21 @@
 package com.amigoscode;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.Month;
+import java.time.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Creating specific Date with LocalDateTime .
+        // Working with ZoneIds .
 
-        LocalDateTime someDate = LocalDateTime.of(
-                1998,
-                Month.OCTOBER,
-                2,
-                10,
-                10
-        );
+        for (String zone : ZoneId.getAvailableZoneIds())
+            System.out.println(zone);
 
-        System.out.println(someDate);
+        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDateTime nowInLondon = LocalDateTime.now(ZoneId.of("Europe/London"));
 
-        // Creating specific Date with LocalDate .
+        System.out.println(localDateTime);
+        System.out.println(nowInLondon);
 
-        LocalDate someDate2 = LocalDate.of(
-                1998,
-                Month.OCTOBER,
-                2
-        );
-
-        System.out.println(someDate2);
-
-        // Creating specific Date with LocalTime .
-
-        LocalTime someTime = LocalTime.of(
-                10,
-                10,
-                55
-        );
-
-        System.out.println(someTime);
 
     }
 
