1773894c45727a820a52b853898972dd7bc85461
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-05 15:52:53 +0530

    What is an exception
    Definition: An exception is an event, which occurs during the execution of a program, that disrupts the normal flow of the program's instructions.

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,33 +1,16 @@
 package com.amigoscode;
 
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        // Scanner
+        // Exceptions
 
-        System.out.println("What's your name?");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println("Hello " + input);
+        int number = Integer.parseInt("1");   // char '1-9' are only allowed
+        System.out.println(number);
 
-        System.out.println("Enter your age :");
-
-        int age = scanner.nextInt();
-
-        if (age < 18)
-            System.out.println("You are a child");
-        else
-            System.out.println("You are adult");
-
-        System.out.println("Enter your average marks :");
-
-        double average = scanner.nextDouble();
-
-        System.out.println("Your average marks is " + average);
-
+        for (int i = 10; i >= 0; --i)
+            System.out.println(10 / i);  // divide by zero
 
     }
 
