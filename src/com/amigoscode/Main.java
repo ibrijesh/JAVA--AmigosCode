267d32f22980d5092d99705e3ba42e70559d946d
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-05 14:49:44 +0530

    Taking User Input -  NextInt and other data types

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -13,6 +13,22 @@ public class Main {
         String input = scanner.nextLine();
         System.out.println("Hello " + input);
 
+        System.out.println("Enter your age :");
+
+        int age = scanner.nextInt();
+
+        if (age < 18)
+            System.out.println("You are a child");
+        else
+            System.out.println("You are adult");
+
+        System.out.println("Enter your average marks :");
+
+        double average = scanner.nextDouble();
+
+        System.out.println("Your average marks is " + average);
+
+
     }
 
 }
