e906b10aee8affad2b75d4c04e24cf6e32b2379f
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-05 15:56:29 +0530

    Try and Catch
    Definition: An exception is an event, which occurs during the execution of a program, that disrupts the normal flow of the program's instructions.

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -6,11 +6,15 @@ public class Main {
     public static void main(String[] args) {
         // Exceptions
 
-        int number = Integer.parseInt("1");   // char '1-9' are only allowed
-        System.out.println(number);
+        try {
+            int number = Integer.parseInt("1x");   // char '1-9' are only allowed
+            System.out.println(number);
+        } catch (NumberFormatException e) {
+            System.out.println("Failed to parse 1x");
+        }
 
-        for (int i = 10; i >= 0; --i)
-            System.out.println(10 / i);  // divide by zero
+//        for (int i = 10; i >= 0; --i)
+//            System.out.println(10 / i);  // divide by zero
 
     }
 
