6783505ea26fe34d7581ab1a35e000fc29a3e341
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-06 14:04:32 +0530

    Exception Hierarchy
    -See photo and video for better understanding

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -6,21 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // Exceptions
 
-        try {
-            int number = Integer.parseInt("1");   // char '1-9' are only allowed
-            System.out.println(number);
 
-            for (int i = 10; i >= 0; --i)
-                System.out.println(10 / i);  // divide by zero
-
-            System.out.println("END");
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        finally {
-            System.out.println("Program Executed Successfully");
-        }
     }
 
 }
