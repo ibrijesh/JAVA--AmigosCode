fc5c44cf7da4d5a4c98f5ceda7e15318d4ccb5b1
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-06 15:47:33 +0530

    Throw and Throws
    Throw - Used to throw exception
    
    Throws - Used to declare exceptions. It specifies that there may occur an exception in the method. It doesn't throw an exception. It is always used with method signature.

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -4,21 +4,43 @@ package com.amigoscode;
 public class Main {
 
     public static void main(String[] args) {
-        // UnChecked Exceptions
+        // Throw and Throws
 
+        System.out.println(divide1(18, 3));
+
+        //  divide2() function may throw Checked Exception, therefore we have cannot run program without handling exception at compile time.
         try {
-            int number = Integer.parseInt("1x");   // char '1-9' are only allowed
-            System.out.println(number);
-
-            for (int i = 10; i >= 0; --i)
-                System.out.println(10 / i);  // divide by zero
-
-            System.out.println("END");
-
+            System.out.println(divide2(4, 0));
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
         }
 
+        System.out.println(divide3(40, 4));
+
     }
+
+    public static int divide1(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Divide by Zero");
+        }
+        return a / b;
+    }
+
+    // may throw Checked exception
+
+    public static int divide2(int a, int b) throws Exception {
+        if (b == 0) {
+            throw new Exception("Divide by Zero");
+        }
+        return a / b;
+    }
+
+    public static int divide3(int a, int b) throws IllegalArgumentException {
+        if (b == 0) {
+            throw new IllegalArgumentException("Divide by Zero");
+        }
+        return a / b;
+    }
+
 
 }
