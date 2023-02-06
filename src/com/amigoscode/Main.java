5dab11b35468a3e4d65d1dd804b61e0351b4e971
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-06 19:57:17 +0530

    Working with Files - Try with Resources
    Any class which Implements Closeable ,Flushable need not to write flush() and close() rather can use Try with resources.

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         // Creating a File
         File file = createFile("src/foo.txt");
-        // writeToFile(file, true, "Cristiano");
+        writeToFile(file, true, "Cristiano");
 
         try {
             Scanner scanner = new Scanner(file);
@@ -22,12 +22,11 @@ public class Main {
     }
 
     private static void writeToFile(File file, boolean append, String content) {
-        try {
-            FileWriter fileWriter = new FileWriter(file, append);
-            PrintWriter writer = new PrintWriter(fileWriter);
-            writer.println(content);
-            writer.flush();
-            writer.close();
+
+        try (FileWriter fileWriter = new FileWriter(file, append);
+             PrintWriter writer = new PrintWriter(fileWriter)
+        ) {
+            writer.println("Ronaldo");
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
