33ed44a25cf0f2b8fdc8a27c5345477411324687
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-06 16:02:25 +0530

    Throwing
    When method do not want to handle exception , it throws the except the part of program where it will be called.

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -4,40 +4,48 @@ package com.amigoscode;
 public class Main {
 
     public static void main(String[] args) {
-        // Throw and Throws
+        // Throwing
 
-        System.out.println(divide1(18, 3));
+        camila(); // camila have handled the exception already in its method.
 
-        //  divide2() function may throw Checked Exception, therefore we have cannot run program without handling exception at compile time.
         try {
-            System.out.println(divide2(4, 0));
+            jamila();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            john();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // camila want to handle the Checked exception on its own.
+    public static void camila() {
+        try {
+            jamila();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-        System.out.println(divide3(40, 4));
-
     }
 
-    public static int divide1(int a, int b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("Divide by Zero");
-        }
-        return a / b;
+    // jamila does not want to handle the Checked exception on its own.
+
+    public static void jamila() throws Exception {
+        john();
     }
 
-    // may throw Checked exception
+    // john want to handle the Checked exception on its own.
 
-    public static int divide2(int a, int b) throws Exception {
+    public static void john() throws Exception {
+        divide(4, 0);
+    }
+
+    public static int divide(int a, int b) throws Exception {
         if (b == 0) {
             throw new Exception("Divide by Zero");
-        }
-        return a / b;
-    }
-
-    public static int divide3(int a, int b) throws IllegalArgumentException {
-        if (b == 0) {
-            throw new IllegalArgumentException("Divide by Zero");
         }
         return a / b;
     }
