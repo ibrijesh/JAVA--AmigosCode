59a51fd8103e64380aca91c2a9010cc207e59227
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-06 16:21:17 +0530

    Creating Custom Exceptions

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -4,48 +4,32 @@ package com.amigoscode;
 public class Main {
 
     public static void main(String[] args) {
-        // Throwing
+        // Creating Custom Exceptions.
 
-        camila(); // camila have handled the exception already in its method.
+        // Unchecked Exception, can be run without ty and catch
+        divide2(4, 0);
 
+
+        //Checked Exception , cannot be run without try and catch
         try {
-            jamila();
-        } catch (Exception e) {
+            divide1(4, 0);
+        } catch (MyCheckedException e) {
             e.printStackTrace();
         }
 
-        try {
-            john();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
     }
 
-    // camila want to handle the Checked exception on its own.
-    public static void camila() {
-        try {
-            jamila();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 
-    // jamila does not want to handle the Checked exception on its own.
-
-    public static void jamila() throws Exception {
-        john();
-    }
-
-    // john want to handle the Checked exception on its own.
-
-    public static void john() throws Exception {
-        divide(4, 0);
-    }
-
-    public static int divide(int a, int b) throws Exception {
+    public static int divide1(int a, int b) throws MyCheckedException {
         if (b == 0) {
-            throw new Exception("Divide by Zero");
+            throw new MyCheckedException("Divide by Zero");
+        }
+        return a / b;
+    }
+
+    public static int divide2(int a, int b) throws MyUnCheckedException {
+        if (b == 0) {
+            throw new MyUnCheckedException("Divide By Zero");
         }
         return a / b;
     }
