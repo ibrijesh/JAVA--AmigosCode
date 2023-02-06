d65a09dbe0a8468c99b075f4f8f0d74dfd11b5cd
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-06 14:15:55 +0530

    Checked Exception -  Compile Time
    Program cannot be run without handling checked exceptions.

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,11 +1,24 @@
 package com.amigoscode;
 
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        // Exceptions
+    public static void main(String[] args)  {
+        // Checked Exceptions  (if not handled program will not run)
 
+        try{
+            File file = new File("src/ABC/foo.txt");   // invalid file path.
+
+            if(!file.exists())
+               file.createNewFile();
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
 
     }
 
