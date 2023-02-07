c7be7f2489bb5fe5fc2402298893cda75fba1568
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-07 17:25:25 +0530

    Multiple Constructor

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -6,12 +6,25 @@ public class Main {
     public static void main(String[] args) {
         // Classes and Objects
 
+        /* Working with Default constructor */
         Cat cat = new Cat();
         cat.setName("Joe");
         cat.setAge(5);
         System.out.println(cat.getName());
         System.out.println(cat.getAge());
         cat.meow();
+
+        /* Working with Parameterized constructor */
+        Cat cat1 = new Cat("Rose", 2);
+        System.out.println(cat1.getName());
+        System.out.println(cat1.getAge());
+        cat1.meow();
+
+
+        Cat cat2 = new Cat("Jupiter");
+        System.out.println(cat2.getName());
+        System.out.println(cat2.getAge());
+        cat2.meow();
 
     }
 
@@ -20,8 +33,18 @@ public class Main {
         private String name;
         private int age;
 
-        // Default constructor, with no argument
+        // Multiple Constructor
         public Cat() {
+        }
+
+        public Cat(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public Cat(String name) {
+            this.name = name;
+            this.age = 0;
         }
 
         public void meow() {
