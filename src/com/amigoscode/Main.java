b2c692c01acc2833af2d789383ef64305b343344
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-07 17:36:41 +0530

    toString()
    The toString method is used to return a string representation of an object.

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -7,24 +7,20 @@ public class Main {
         // Classes and Objects
 
         /* Working with Default constructor */
-        Cat cat = new Cat();
-        cat.setName("Joe");
-        cat.setAge(5);
-        System.out.println(cat.getName());
-        System.out.println(cat.getAge());
-        cat.meow();
+        Cat joe = new Cat();
+        joe.setName("Joe");
+        joe.setAge(5);
+        joe.setColor("Black");
+        System.out.println(joe);
+
 
         /* Working with Parameterized constructor */
-        Cat cat1 = new Cat("Rose", 2);
-        System.out.println(cat1.getName());
-        System.out.println(cat1.getAge());
-        cat1.meow();
+        Cat rose = new Cat("Rose", 2, "Blue");
+        System.out.println(rose);
 
 
-        Cat cat2 = new Cat("Jupiter");
-        System.out.println(cat2.getName());
-        System.out.println(cat2.getAge());
-        cat2.meow();
+        Cat jupiter = new Cat("Jupiter");
+        System.out.println(jupiter);
 
     }
 
@@ -32,14 +28,16 @@ public class Main {
     static class Cat {
         private String name;
         private int age;
+        private String color;
 
         // Multiple Constructor
         public Cat() {
         }
 
-        public Cat(String name, int age) {
+        public Cat(String name, int age, String color) {
             this.name = name;
             this.age = age;
+            this.color = color;
         }
 
         public Cat(String name) {
@@ -63,10 +61,29 @@ public class Main {
             return age;
         }
 
+        public String getColor() {
+            return color;
+        }
+
         public void setAge(int age) {
             this.age = age;
         }
 
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        /* toString */
+
+        @Override
+        public String toString() {
+            return "Cat{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    ", color='" + color + '\'' +
+                    '}';
+        }
     }
 
 }
