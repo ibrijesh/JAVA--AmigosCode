1f861ba1e3e031563b0b8ac9cf1cd144f563881d
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-07 16:27:24 +0530

    Your First Class
    OuterClass can access Inner Class private members.
    
    without static keyword inner class object cannot be created.

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,10 +1,25 @@
 package com.amigoscode;
 
-
+// outer class
 public class Main {
 
     public static void main(String[] args) {
         // Classes and Objects
+
+        Cat cat = new Cat();
+        cat.name = "Joe";
+        cat.meow();
+
+    }
+
+    // inner class
+    static class Cat {
+        private String name;
+
+        public void meow() {
+            System.out.println(name + ": meow...");
+        }
+
 
     }
 
