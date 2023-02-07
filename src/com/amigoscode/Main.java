3f408f3c49c988fe8e9e6d9cc2c77aafd005023f
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-07 17:15:14 +0530

    Constructor

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -6,21 +6,27 @@ public class Main {
     public static void main(String[] args) {
         // Classes and Objects
 
-        Cat cat = new Cat();
-        cat.setName("Joe");
+        Cat cat = new Cat("Rose", 2);
         System.out.println(cat.getName());
+        System.out.println(cat.getAge());
         cat.meow();
-
-        Cat cat1 = new Cat();
-        cat1.setName("Jimmy");
-        System.out.println(cat1.getName());
-        cat1.meow();
 
     }
 
     // inner class
     static class Cat {
         private String name;
+        private int age;
+
+        // constructor
+        public Cat(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void meow() {
+            System.out.println(name + ": meow...");
+        }
 
         public void setName(String name) {
             this.name = name;
@@ -30,8 +36,12 @@ public class Main {
             return name;
         }
 
-        public void meow() {
-            System.out.println(name + ": meow...");
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
         }
 
     }
