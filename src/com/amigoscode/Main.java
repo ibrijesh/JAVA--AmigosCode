5bed18c6ed20b77036142ba22025ce8b36dce15c
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-11 13:54:01 +0530

    Package (Directory or Folder)

--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,24 +1,18 @@
 package com.amigoscode;
 
-import static java.lang.Math.*;
 
 public class Main {
 
-    public static Person[] PEOPLE = null;
-
-    static {
-        System.out.println("static initialization in main");
-        PEOPLE = new Person[10];
-
-        // business logic
-    }
 
     public static void main(String[] args) {
-        // Static Keyword
+        // Organize Code
 
-        System.out.println(max(10, 20));
-        System.out.println(min(10, 20));
-        System.out.println(sqrt(16));
+        // Packages
+        // Domain / Model
+        // Services Classes -> business logic
+        // DAO Classes
+        // Utility classes
+        // Configuration class
 
     }
 
---
+++ src/com/amigoscode/car/Car.java
@@ -0,0 +1,33 @@
+package com.amigoscode.car;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Car implements Serializable {
+    private String regNumber;
+    private BigDecimal price;
+
+    public Car() {
+    }
+
+    public Car(String regNumber, BigDecimal price) {
+        this.regNumber = regNumber;
+        this.price = price;
+    }
+
+    public String getRegNumber() {
+        return regNumber;
+    }
+
+    public void setRegNumber(String regNumber) {
+        this.regNumber = regNumber;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
