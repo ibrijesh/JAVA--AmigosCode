fbfc1707a0192a6fb56f7fc13191224076e0932b
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-11 16:00:05 +0530

    The Problem with Non-Private Fields.
    Non-Private can be manipulated by external entity.

--- src/com/amigoscode/BankAccount.java
+++ src/com/amigoscode/BankAccount.java
@@ -9,6 +9,21 @@ public class BankAccount {
     BigDecimal balance;
     boolean hasOverDraft;
 
+    public BankAccount() {
+    }
+
+    public BankAccount(String name, BigDecimal balance, boolean hasOverDraft) {
+        this.name = name;
+        this.balance = balance;
+        this.hasOverDraft = hasOverDraft;
+    }
+
+    public BankAccount(String name) {
+        this.name = name;
+        this.balance=ZERO;
+        this.hasOverDraft=false;
+    }
+
     public BigDecimal withdraw(BigDecimal amount) {
         if (balance.subtract(amount).compareTo(ZERO) >= 0) {
             this.balance = this.balance.subtract(amount);
--- src/com/amigoscode/Main.java
+++ src/com/amigoscode/Main.java
@@ -1,12 +1,30 @@
 package com.amigoscode;
 
 
+import java.math.BigDecimal;
+
 public class Main {
 
     public static void main(String[] args) {
         // OOPs-Encapsulation
 
+        BankAccount account = new BankAccount("Amigos", BigDecimal.TEN, false);
+        System.out.println(
+                account.withdraw(new BigDecimal("9.0"))
+        );
 
+        System.out.println(account.balance); // 1.0
+
+        System.out.println(
+                account.withdraw(new BigDecimal("1.0"))
+        );
+
+        System.out.println(account.balance); //0.0
+
+        /* but system is manipulated */
+        account.balance = new BigDecimal("100.0");
+
+        System.out.println(account.balance);
 
 
     }
