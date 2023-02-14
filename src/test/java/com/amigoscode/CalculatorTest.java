a3a411392be4b269398c6c4efd47257fd2b2ad68
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-14 16:02:16 +0530

    Writing Your First Unit Test.

--- src/test/java/com/amigoscode/CalculatorTest.java
+++ src/test/java/com/amigoscode/CalculatorTest.java
@@ -1,6 +1,19 @@
 package com.amigoscode;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
 
+
+    @Test
+    void canTwoAddNumbers() {
+        Calculator underTest = new Calculator();
+        int number1 = 10;
+        int number2 = 20;
+        int result = underTest.add(number1, number2);
+        assertEquals(30, result);
+    }
 
 }
