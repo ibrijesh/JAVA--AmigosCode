c0f1f6ae8bd7305a89bb988d7596fd493c081da2
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-14 13:34:57 +0530

    Installing JUnit

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -1,10 +1,17 @@
 package com.amigoscode;
 
 
+import com.github.javafaker.Faker;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Maven");
+
+        Faker faker = new Faker();
+        System.out.println(faker.name().fullName());
+        System.out.println(faker.address().city());
+
     }
 
 }
