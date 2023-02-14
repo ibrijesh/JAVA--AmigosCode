d351c8aedc840c64965b015d21abf73c21d423a0
Brijesh Yadav <brijeshyadavjthj@gmail.com>
2023-02-14 16:09:46 +0530

    Test Scenarios

--- src/test/java/com/amigoscode/CalculatorTest.java
+++ src/test/java/com/amigoscode/CalculatorTest.java
@@ -16,4 +16,20 @@ public class CalculatorTest {
         assertEquals(30, result);
     }
 
+    @Test
+    void canHandleZero() {
+        Calculator underTest = new Calculator();
+        int result = underTest.add(0);
+        assertEquals(0, result);
+    }
+
+    @Test
+    void canHandleNegativeNumbers() {
+        Calculator underTest = new Calculator();
+        int number1 = -10;
+        int number2 = -20;
+        int result = underTest.add(number1, number2);
+        assertEquals(-30, result);
+    }
+
 }
